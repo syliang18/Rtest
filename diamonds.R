ca3b76727970a1c302291bc6ceec667cfcfa65e6
syliang18 <syliang18@gmail.com>
2023-09-23 17:41:45 +0800

    add a comments to pdf

--- diamonds.R
+++ diamonds.R
@@ -2,7 +2,7 @@ library(tidyverse)
 # make a plot
 ggplot(diamonds, aes(carat, price)) + 
   geom_hex()
-# save a plot
+# save a plot to pdf
 ggsave("diamonds.pdf")
 
 write_csv(diamonds, "diamonds.csv")
